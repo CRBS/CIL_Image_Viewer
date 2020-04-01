9cf5adb4e3653f6c0504fd34fe6625e2d54d180f
Willy Wong <wawong@gmail.com>
2020-03-31 23:04:43 -0700

    Added the Google Colab instructions

--- application/views/cdeep3m/view_cdeep3m_result_display.php
+++ application/views/cdeep3m/view_cdeep3m_result_display.php
@@ -729,7 +729,7 @@
                     If you are comfortable using a command line interface, this provides the most flexible way to use all functionality of CDeep3M while using Googles free GPUs. It performs the complete CDeep3M installation and sets you up with the CDeep3M CLI on colab.
                </div>
               <div class="col-md-12">
-                  <a href="https://colab.research.google.com/github/haberlmatt/cdeep3m-colab/blob/master/CDeep3M_V2_installation_and_CLI.ipynb" rel="nofollow"><img src="https://camo.githubusercontent.com/52feade06f2fecbf006889a904d221e6a730c194/68747470733a2f2f636f6c61622e72657365617263682e676f6f676c652e636f6d2f6173736574732f636f6c61622d62616467652e737667" data-canonical-src="https://colab.research.google.com/assets/colab-badge.svg" style="max-width:100%;" align="middle"></a>
+                  <a href="https://colab.research.google.com/github/haberlmatt/cdeep3m-colab/blob/master/CDeep3M_V2_installation_and_CLI.ipynb" rel="nofollow" target="_blank"><img src="https://camo.githubusercontent.com/52feade06f2fecbf006889a904d221e6a730c194/68747470733a2f2f636f6c61622e72657365617263682e676f6f676c652e636f6d2f6173736574732f636f6c61622d62616467652e737667" data-canonical-src="https://colab.research.google.com/assets/colab-badge.svg" style="max-width:100%;" align="middle"></a>
               </div>
               <div class="col-md-12">
                   <br/>
