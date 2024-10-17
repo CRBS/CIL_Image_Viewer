fb78e30bee3efaeba6ee9256235e20ea1a18159d
Willy at NCMIR <wawong@gmail.com>
2024-10-17 00:52:53 -0700

    Working on editing the NCMIR metadata

---
+++ application/controllers/Ncmir_metadata.php
@@ -0,0 +1,60 @@
+<?php
+    include_once 'PasswordHash.php';
+    require_once 'GeneralUtil.php';
+    require_once 'DBUtil.php';
+    require_once 'DataLocalUtil.php';
+    require_once 'Constants.php';
+    require_once 'MailUtil.php';
+    
+    class Ncmir_metadata extends CI_Controller
+    {
+        public function edit($mpid="0")
+        {
+            $this->load->helper('url');
+            $dbutil = new DBUtil();
+            
+            $base_url = $this->config->item('base_url');
+            $data['enable_augmentation'] = $this->config->item('enable_augmentation');
+            $data['cdeep3m_website_url'] = $this->config->item('cdeep3m_website_url');
+            $cil_pgsql_db = $this->config->item('cil_pgsql_db');
+            
+            $data['base_url'] = $base_url;
+            
+            $token = $this->input->get('token', TRUE);
+            $username = $this->input->get('username', TRUE);
+            $data['username'] = $username;
+            if(!is_null($username) && !is_null($token))
+            {
+                $ip_address = $this->input->ip_address();
+                $this->authenticateByToken($username, $token,$ip_address);
+            }
+            /********Session check**************************/
+            $data_login = $this->session->userdata('data_login');
+            if(is_null($data_login))
+            {
+                show_404();
+                //echo "404 1";
+                
+                return;
+            }
+            /********End session check **********************/
+            
+            
+            /***********Getting user_json*******************/
+            $user_json  = $this->session->userdata('user_json');
+            //echo $user_json->full_name;
+            
+            if(!is_null($user_json))
+                $data['user_json'] = $user_json;
+            else
+            { 
+                show_404();
+                //echo "404 2";
+                return;
+            }
+            /***********End user_json*******************/
+            
+            $this->load->view('ncmir_metadata/edit_metadata_display', $data);
+        }
+    }
+
